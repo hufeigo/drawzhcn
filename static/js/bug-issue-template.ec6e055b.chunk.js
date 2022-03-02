bfb17d7ba3049f389093948ab3b0864fc1bb7008
hufeigo <hufei@foxmail.com>
2022-03-02 14:13:22 +0800

    Add files via upload
    fisrt

---
+++ static/js/bug-issue-template.ec6e055b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpexcalidraw=this.webpackJsonpexcalidraw||[]).push([[2],{211:function(n,e,t){"use strict";t.r(e),e.default=function(n){return"\n### Scene content\n\n```\nPaste scene content here\n```\n\n### Sentry Error ID\n\n".concat(n,"\n")}}}]);
+//# sourceMappingURL=bug-issue-template.ec6e055b.chunk.js.map
---
+++ static/js/socketIoClient.d8ba704f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpexcalidraw=this.webpackJsonpexcalidraw||[]).push([[9],{204:function(a,c){}}]);
+//# sourceMappingURL=socketIoClient.d8ba704f.chunk.js.map
